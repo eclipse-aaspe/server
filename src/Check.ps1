ad478421ec279f885539e7a67aba5b464d3397b4
Marko Ristin <marko.ristin@gmail.com>
2020-07-27 09:32:40 +0200

    Check commit messages (#7)
    This patch adds a checker for commit messages to the continuous
    integration.

---
+++ src/Check.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+This script runs all the pre-merge checks locally.
+#>
+
+$ErrorActionPreference = "Stop"
+
+function Main
+{
+    Set-Location $PSScriptRoot
+    .\CheckPushCommitMessages.ps1
+}
+
+$previousLocation = Get-Location; try { Main } finally { Set-Location $previousLocation }
