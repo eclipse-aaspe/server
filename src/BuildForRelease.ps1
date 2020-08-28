c40d0ff94e384f41c81b1c17ec839171d4354d8b
Marko Ristin <marko.ristin@gmail.com>
2020-08-28 18:09:34 +0200

    Set up CI for building and releasing
    This is an initial set up of the continuous integration that builds and
    releases the solution.

---
+++ src/BuildForRelease.ps1
@@ -0,0 +1,46 @@
+﻿param(
+    [Parameter(HelpMessage = "If set, cleans up the previous build instead of performing a new one")]
+    [switch]
+    $clean = $false
+)
+
+<#
+.DESCRIPTION
+This script builds the solution for debugging (manual or automatic testing).
+#>
+
+$ErrorActionPreference = "Stop"
+
+Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
+    AssertDotnet, `
+    GetArtefactsDir
+
+function Main
+{
+    Set-Location $PSScriptRoot
+
+    $buildDir = Join-Path $( GetArtefactsDir ) "build" `
+        | Join-Path -ChildPath "Release" `
+        | Join-Path -ChildPath "aasx-server"
+
+    if ($clean)
+    {
+        Write-Host "Removing the build directory: $buildDir"
+        Remove-Item -LiteralPath $buildDir -Force -Recurse
+    }
+    else
+    {
+        AssertDotnet
+        Write-Host "Publishing the solution to: $buildDir"
+        dotnet.exe publish -c Release -o $buildDir
+    }
+}
+
+$previousLocation = Get-Location; try
+{
+    Main
+}
+finally
+{
+    Set-Location $previousLocation
+}
---
+++ src/PackageRelease.ps1
@@ -0,0 +1,89 @@
+param(
+    [Parameter(HelpMessage = "Version to be packaged", Mandatory = $true)]
+    [string]
+    $version
+)
+
+<#
+.SYNOPSIS
+This script packages files to be released.
+#>
+
+$ErrorActionPreference = "Stop"
+
+Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
+    GetArtefactsDir
+
+function PackageRelease($outputDir)
+{
+    $buildDir = Join-Path $( GetArtefactsDir ) "build" `
+        | Join-Path -ChildPath "Release" `
+        | Join-Path -ChildPath "aasx-server"
+
+    if (!(Test-Path $buildDir))
+    {
+        throw ("The build directory with the release does " +
+                "not exist: $buildDir; did you build the solution " +
+                "with BuildForRelease.ps1?")
+    }
+
+    New-Item -ItemType Directory -Force -Path $outputDir|Out-Null
+
+    $archPath = Join-Path $outputDir "aasx-server.zip"
+
+    Write-Host "Compressing to: $archPath"
+
+    Compress-Archive `
+        -Path $buildDir `
+        -DestinationPath $archPath
+
+    # Do not copy the source code in the releases.
+    # The source code will be distributed automatically through Github releases.
+
+    Write-Host "Done packaging the release."
+}
+
+function Main
+{
+    if ($version -eq "")
+    {
+        throw "Unexpected empty version"
+    }
+
+    $versionRe = [Regex]::new(
+            '^[0-9]{4}-(0[1-9]|10|11|12)-(0[1-9]|1[0-9]|2[0-9]|3[0-1])' +
+            '(\.(alpha|beta))?$')
+
+    $latestVersionRe = [Regex]::new('^LATEST(\.(alpha|beta))?$')
+
+    if ((!$latestVersionRe.IsMatch($version)) -and
+            (!$versionRe.IsMatch($version)))
+    {
+        throw ("Unexpected version; " +
+                "expected either year-month-day (*e.g.*, 2019-10-23) " +
+                "followed by an optional maturity tag " +
+                "(*e.g.*, 2019-10-23.alpha) " +
+                "or LATEST, but got: $version")
+    }
+
+    $outputDir = Join-Path $( GetArtefactsDir ) "release" `
+        | Join-Path -ChildPath $version
+
+    if (Test-Path $outputDir)
+    {
+        Write-Host ("Removing previous release so that " +
+                "the new release is packaged clean: $outputDir")
+        Remove-Item -Recurse -Force $outputDir
+    }
+
+    PackageRelease -outputDir $outputDir
+}
+
+$previousLocation = Get-Location; try
+{
+    Main
+}
+finally
+{
+    Set-Location $previousLocation
+}
