48c44a4d8c0c0f09086394753920ca528195a621
Marko Ristin <marko.ristin@gmail.com>
2020-08-28 18:15:30 +0200

    Set up CI for building and releasing (#12)
    This is an initial set up of the continuous integration that builds and
    releases the solution.

---
+++ src/Common.psm1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+This module contains common functions for continuous integration.
+#>
+
+<#
+.SYNOPSIS
+Asserts that dotnet is on the path.
+#>
+function AssertDotnet
+{
+    if (!(Get-Command "dotnet" -ErrorAction SilentlyContinue))
+    {
+        if ($null -eq $env:LOCALAPPDATA)
+        {
+            throw "dotnet could not be found in the PATH."
+        }
+        else
+        {
+            throw "dotnet could not be found in the PATH. Look if you could find it, e.g., in " + `
+               "$( Join-Path $env:LOCALAPPDATA "Microsoft\dotnet" ) and add it to PATH."
+        }
+    }
+}
+
+function GetArtefactsDir
+{
+    $repoRoot = Split-Path $PSScriptRoot -Parent
+    $artefactsDir = Join-Path $repoRoot "artefacts"
+    return $artefactsDir
+}
+
+Export-ModuleMember -Function `
+    AssertDotnet, `
+    GetArtefactsDir
