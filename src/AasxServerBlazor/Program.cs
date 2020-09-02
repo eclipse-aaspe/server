f8b3b73223d058be162cd276bf0a2af5ee3fcff9
Andreas Orzelski <aorzelski@phoenixcontact.com>
2020-09-02 08:15:28 +0200

    Rename and move AasxBlazor to AasxServerBlazor

---
+++ src/AasxServerBlazor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AasxServerBlazor
+{
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            CreateHostBuilder(args).Build().RunAsync();
+
+            Net46ConsoleServer.Program.Main(args);            
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseUrls("http://*:5000");
+                });
+    }
+}
