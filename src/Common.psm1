7f01d6a32fcc5ab2c7635dc90b28a6dbf2dbbd4c
Marko Ristin <marko.ristin@gmail.com>
2020-09-04 11:17:19 +0200

    Introduce dotnet-format to continuous integration (#24)
    This pactch includes scripts that re-format the code and adds format
    check to the continuous integration.
    
    The workflow build-and-publish-docker-images was intentionally skipped.
    The workflow check-release was intentionally skipped.

--- src/Common.psm1
+++ src/Common.psm1
@@ -23,6 +23,69 @@ function AssertDotnet
     }
 }
 
+function FindDotnetToolVersion($PackageID) {
+    AssertDotnet
+
+    $version = ''
+
+    $lines = (dotnet tool list)|Select-Object -Skip 2
+    $lines += (dotnet tool list -g)|Select-Object -Skip 2
+    ForEach ($line in $( $lines -split "`r`n" ))
+    {
+        $parts = $line -Split '\s+'
+        if ($parts.Count -lt 3)
+        {
+            throw "Expected at least 3 columns in a line of `dotnet tool list`, got output: ${lines}"
+        }
+
+        $aPackageID = $parts[0]
+        $aPackageVersion = $parts[1]
+
+        if ($aPackageID -eq $PackageID)
+        {
+            $version = $aPackageVersion
+            break
+        }
+    }
+
+    return $version
+}
+
+<#
+.SYNOPSIS
+Check the version of the given dotnet tool.
+ #>
+function AssertDotnetToolVersion($PackageID, $ExpectedVersion) {
+    AssertDotnet
+
+    $version = FindDotnetToolVersion -PackageID $PackageID
+    if ($version -eq '')
+    {
+        throw "No $PackageID could be found. Have you installed it? " + `
+               "Check the list of the installed dotnet tools with: " + `
+               "`dotnet tool list` and `dotnet tool list -g`."
+    }
+    else
+    {
+        if ($version -ne $ExpectedVersion)
+        {
+            throw "Expected $PackageID version $ExpectedVersion, but got: $version;" + `
+                   "Check the list of the installed dotnet tools with: " + `
+                   "`dotnet tool list` and `dotnet tool list -g`."
+        }
+        # else: the version is correct.
+    }
+}
+
+<#
+.SYNOPSIS
+Check the version of dotnet-format so that the code is always formatted in the same manner.
+#>
+function AssertDotnetFormatVersion
+{
+    AssertDotnetToolVersion -packageID "dotnet-format" -expectedVersion "4.0.130203"
+}
+
 function GetArtefactsDir
 {
     $repoRoot = Split-Path $PSScriptRoot -Parent
@@ -32,4 +95,5 @@ function GetArtefactsDir
 
 Export-ModuleMember -Function `
     AssertDotnet, `
+    AssertDotnetFormatVersion, `
     GetArtefactsDir
