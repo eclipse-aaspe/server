8c741a8378cc19cc0c87ce11b299c84e237fe8b8
magnus redeker <magnus.redeker@iosb-ina.fraunhofer.de>
2020-10-05 14:48:51 +0200

    adapted AasxServerStandardBib/AasxHttpContextHelper.cs; added AasxServerStandardBib/AasxFileInfo.cs; added AasxServerStandardBib/FindAasReturn.cs

---
+++ src/AasxServerStandardBib/FindAasReturn.cs
@@ -0,0 +1,10 @@
+﻿using AdminShellNS;
+
+namespace AasxRestServerLibrary
+{
+    public class FindAasReturn
+    {
+        public AdminShell.AdministrationShell aas = null;
+        public int iPackage = -1;
+    }
+}
