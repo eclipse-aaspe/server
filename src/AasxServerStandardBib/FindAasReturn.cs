20764ce0d6893aa135dd9a925e2320a4ffdd9047
magnus-redeker <71870532+magnus-redeker@users.noreply.github.com>
2020-10-20 18:46:21 +0200

    Add itsowl changes for REST put of AAS
    * added RestRoutes in AasxServerStandardBib/AasxRestServer.cs
    
    * branch-info in README.md
    
    * adapted AasxServerStandardBib/AasxHttpContextHelper.cs; added AasxServerStandardBib/AasxFileInfo.cs; added AasxServerStandardBib/FindAasReturn.cs
    
    * adapted AasxServerStandardBib/AdminShell.cs
    
    * adapted AasxServerStandardBib/AdminShellPackageEnv.cs
    
    * removed branch-info from README.md
    
    * fixed whitespace formatting
    
    * auto property in FindAasReturn

---
+++ src/AasxServerStandardBib/FindAasReturn.cs
@@ -0,0 +1,10 @@
+﻿using AdminShellNS;
+
+namespace AasxRestServerLibrary
+{
+    public class FindAasReturn
+    {
+        public AdminShell.AdministrationShell aas { get; set; } = null;
+        public int iPackage { get; set; } = -1;
+    }
+}
