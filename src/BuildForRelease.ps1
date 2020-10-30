d4dc2615e88d342a5609dbf1a1c56cfa0a080a9b
Marko Ristin <marko.ristin@gmail.com>
2020-10-30 13:02:54 +0100

    Convert AasxServerWindows project to SDK-style
    This patch converts the AasxServerWindows project from legacy (non-SDK)
    to SDK style. This makes it easier to build & publish the releases as
    well as to manage the dependencies.
    
    The config `src/AasxServerWindows/App.config` seemed unnecessary and
    conflicting with the actual versions that `dotnet build` used, so it was
    removed. As it seems, it pinned only the library versions.

--- src/BuildForRelease.ps1
+++ src/BuildForRelease.ps1
@@ -15,64 +15,6 @@ Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function `
     AssertDotnet, `
     GetArtefactsDir
 
-<#
-.SYNOPSIS
-Search for MSBuild in the path and at expected locations using `vswhere.exe`.
-#>
-function FindMSBuild
-{
-    $msbuild = $null
-
-    $msbuildCommand = Get-Command "MSBuild.exe" -ErrorAction SilentlyContinue
-    $msbuildFailedSearches = @()
-    if ($null -ne $msbuildCommand)
-    {
-        $msbuild = $msbuildCommand.Source
-    }
-    else
-    {
-        $vswherePath = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-        if (!(Test-Path $vswherePath))
-        {
-            throw "Could not find vswhere at: $vswherePath"
-        }
-
-        $ids = 'Community', 'Professional', 'Enterprise', 'BuildTools' `
-            | ForEach-Object { 'Microsoft.VisualStudio.Product.' + $_ }
-
-        $instance = & $vswherePath -latest -products $ids -requires Microsoft.Component.MSBuild -format json `
-            | Convertfrom-Json `
-            | Select-Object -first 1
-
-        $msbuildPath = Join-Path $instance.installationPath 'MSBuild\15.0\Bin\MSBuild.exe'
-        if (Test-Path $msbuildPath)
-        {
-            $msbuild = $msbuildPath
-        }
-        else
-        {
-            $msbuildFailedSearches += $msbuildPath
-
-            $msbuildPath = Join-Path $instance.installationPath 'MSBuild\Current\Bin\MSBuild.exe'
-            if (Test-Path $msbuildPath)
-            {
-                $msbuild = $msbuildPath
-            }
-            else
-            {
-                $msbuildFailedSearches += $msbuildPath
-            }
-        }
-    }
-
-    if (!$msbuild)
-    {
-        throw "Could not find MSBuild in PATH and at these locations: $( $msbuildFailedSearches -join ';' )"
-    }
-
-    return $msbuild
-}
-
 function CopyContentForDemo($Destination)
 {
     $contentForDemoDir = Join-Path (Split-Path -Parent $PSScriptRoot) "content-for-demo"
@@ -117,6 +59,7 @@ function Main
         $dotnetTargets = $(
         "AasxServerBlazor"
         "AasxServerCore"
+        "AasxServerWindows"
         )
 
         foreach ($target in $dotnetTargets)
@@ -133,42 +76,6 @@ function Main
 
             CopyContentForDemo -Destination $buildDir
         }
-
-        ##
-        # Build AasxServerWindows with MSBuild
-        #
-        # This is necessary as AasxServerWindows depends on
-        # .NET Framework 4.7.2.
-        ##
-
-        if ($null -eq (Get-Command "nuget.exe" -ErrorAction SilentlyContinue))
-        {
-           throw "Unable to find nuget.exe in your PATH"
-        }
-
-        $msbuild = FindMSBuild
-
-        $target = "AasxServerWindows"
-
-        Write-Host "Restoring NuGet dependencies for $target ..."
-        nuget.exe restore $target -PackagesDirectory packages
-
-        $buildDir = Join-Path $baseBuildDir $target
-        Write-Host "Building with MSBuild $target to: $buildDir"
-        & $msbuild `
-            "/p:OutputPath=$buildDir" `
-            "/p:Configuration=Release" `
-            "/p:Platform=x64" `
-            /maxcpucount `
-            $target `
-            /t:build
-
-        if ($LASTEXITCODE -ne 0)
-        {
-            throw "Failed to MSBuild $target."
-        }
-
-        CopyContentForDemo -Destination $buildDir
     }
 }
 
