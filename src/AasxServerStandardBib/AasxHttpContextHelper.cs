df5c513149eaa01fc7e460ecb3e3dd26d9db50fe
Hoffmeister <miho@festo.net>
2021-01-03 12:41:55 +0100

    Work on /property query
    * made it recursive
      (element-names are now pathes)
    * JSONfy it (no text syntax)
    * refactor source code
    * extend to Submodels as well

--- src/AasxServerStandardBib/AasxHttpContextHelper.cs
+++ src/AasxServerStandardBib/AasxHttpContextHelper.cs
@@ -1829,7 +1829,7 @@ namespace AasxRestServerLibrary
             {
                 context.Response.SendResponse(HttpStatusCode.NotFound, $"No AAS '{aasid}' or no Submodel with idShort '{smid}' found.");
                 return;
-            }
+            }                    
 
             // Submodel or SME?
             if (elemids == null || elemids.Length < 1)
