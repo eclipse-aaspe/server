1f13af56e26e61a1bfad52d6496bf1b5202e177b
aorzelskiGH <48956800+aorzelskiGH@users.noreply.github.com>
2022-04-09 18:21:57 +0200

    Undo change

--- src/AasxServerBlazor/startForDemo.sh
+++ src/AasxServerBlazor/startForDemo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet AasxServerBlazor.dll --rest --no-security --data-path ./aasxs --host 0.0.0.0 $AASX-SERVER-OPTIONS
+dotnet AasxServerBlazor.dll --rest --no-security --data-path ./aasxs --host 0.0.0.0
