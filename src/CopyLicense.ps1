a2a5f512f15730bc519670d0dde12d7872e08cde
Andreas Orzelski <aorzelski@phoenixcontact.com>
2022-07-25 08:24:44 +0200

    Add TreeComponent (Tree.razor, treeStyles.css) to LICENSE.TXT
    New script CopyLicense.ps1 for LICENSE.TXT updates

---
+++ src/CopyLicense.ps1
@@ -0,0 +1,5 @@
+copy ../LICENSE.TXT AasxServerBlazor
+copy ../LICENSE.TXT AasxServerBlazor/wwwroot
+copy ../LICENSE.TXT AasxServerCore
+copy ../LICENSE.TXT AasxServerStandardBib
+copy ../LICENSE.TXT AasxServerWindows
