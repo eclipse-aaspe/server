02a89b18a25f78ab92189355b07079d4444086e5
Andreas Orzelski <aorzelski@phoenixcontact.com>
2022-07-26 12:30:21 +0200

    Add generic XML importer
    Base importer on PCN import
    Improve AAS blazor view for qualifier

--- src/AasxServerStandardBib/ProductChange.cs
+++ src/AasxServerStandardBib/ProductChange.cs
@@ -20,6 +20,7 @@ namespace ProductChange
         public static DateTime lastRun = new DateTime();
         public static void pcnInit()
         {
+            xmlImport();
             pcnThread = new Thread(new ThreadStart(pcnLoop));
             pcnThread.Start();
         }
@@ -404,6 +405,207 @@ namespace ProductChange
                 }
             }
             Thread.Sleep(5 * 60 * 1000);
+        }
+
+        static void xmlImport()
+        {
+            Console.WriteLine("Importing xml files from ./xml to submodel xml");
+
+            AdminShellPackageEnv xmlEnv = null;
+            AdminShell.Submodel xmlSub = null;
+            AdminShell.SubmodelElementCollection imported = null;
+            int aascount = AasxServer.Program.env.Length;
+
+            for (int i = 0; i < aascount; i++)
+            {
+                var env = AasxServer.Program.env[i];
+                if (env != null)
+                {
+                    var aas = env.AasEnv.AdministrationShells[0];
+                    if (aas.submodelRefs != null && aas.submodelRefs.Count > 0)
+                    {
+                        foreach (var smr in aas.submodelRefs)
+                        {
+                            var sm = env.AasEnv.FindSubmodel(smr);
+                            if (sm != null && sm.idShort != null && sm.idShort.ToLower().Contains("xml"))
+                            {
+                                xmlEnv = env;
+                                xmlSub = sm;
+
+                                foreach (var sme in sm.submodelElements)
+                                {
+                                    if (sme.submodelElement is AdminShell.SubmodelElementCollection smc)
+                                    {
+                                        if (smc.idShort == "imported")
+                                        {
+                                            imported = smc;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (imported == null)
+                                {
+                                    imported = AdminShell.SubmodelElementCollection.CreateNew("imported");
+                                    sm.Add(imported);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (xmlEnv == null || xmlSub == null || imported == null)
+                return;
+            // if (!Directory.Exists("./xml"))
+            {
+                DateTime timeStamp = DateTime.UtcNow;
+                System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo("./xml");
+                foreach (System.IO.FileInfo fi in ParentDirectory.GetFiles("*.xml"))
+                {
+                    string name = fi.Name;
+                    var c = AdminShell.SubmodelElementCollection.CreateNew(name);
+                    c.TimeStampCreate = timeStamp;
+                    c.setTimeStamp(timeStamp);
+                    /*
+                    var f = AdminShell.File.CreateNew(name);
+                    f.TimeStampCreate = timeStamp;
+                    f.setTimeStamp(timeStamp);
+                    f.value = "/aasx/" + name;
+                    c.Add(f);
+                    */
+                    xmlSub.Add(c);
+
+                    var reader = new XmlTextReader(fi.DirectoryName + "/" + name);
+                    int stack = -1;
+                    string[] readerName = new string[100];
+                    string[] readerValue = new string[100];
+                    AdminShell.SubmodelElementCollection[] smc =
+                        new AdminShell.SubmodelElementCollection[100];
+                    while (reader != null && reader.Read())
+                    {
+                        // if (emptyElements.Contains(reader.Name))
+                        //    continue;
+
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element: // The node is an element.
+                                if (reader.IsEmptyElement)
+                                    break;
+                                if (stack >= 0 && smc[stack] == null)
+                                {
+                                    smc[stack] = AdminShell.SubmodelElementCollection.CreateNew(readerName[stack]);
+                                    smc[stack].TimeStampCreate = timeStamp;
+                                    smc[stack].setTimeStamp(timeStamp);
+                                    if (stack == 0)
+                                    {
+                                        if (c != null)
+                                            c.Add(smc[stack]);
+                                    }
+                                    if (stack > 0)
+                                    {
+                                        if (smc[stack - 1] != null)
+                                            smc[stack - 1].Add(smc[stack]);
+                                    }
+                                }
+                                stack++;
+                                if (stack >= 0 && stack < 100)
+                                {
+                                    if (reader.HasAttributes)
+                                    {
+                                        smc[stack] = AdminShell.SubmodelElementCollection.CreateNew(reader.Name);
+                                        smc[stack].TimeStampCreate = timeStamp;
+                                        smc[stack].setTimeStamp(timeStamp);
+                                        AdminShell.Qualifier q = new AdminShellV20.Qualifier();
+                                        q.type = "XmlHasAttributes";
+                                        smc[stack].qualifiers = new AdminShellV20.QualifierCollection();
+                                        smc[stack].qualifiers.Add(q);
+                                        if (stack == 0)
+                                        {
+                                            if (c != null)
+                                                c.Add(smc[stack]);
+                                        }
+                                        if (stack > 0)
+                                        {
+                                            if (smc[stack - 1] != null)
+                                                smc[stack - 1].Add(smc[stack]);
+                                        }
+
+                                        for (int i = 0; i < reader.AttributeCount; i++)
+                                        {
+                                            reader.MoveToAttribute(i);
+                                            string n = reader.Name;
+                                            string v = reader.Value;
+                                            var p = AdminShell.Property.CreateNew(n);
+                                            p.TimeStampCreate = timeStamp;
+                                            p.setTimeStamp(timeStamp);
+                                            p.valueType = "string";
+                                            p.value = v;
+                                            q = new AdminShellV20.Qualifier();
+                                            q.type = "XmlAttribute";
+                                            p.qualifiers = new AdminShellV20.QualifierCollection();
+                                            p.qualifiers.Add(q);
+                                            if (smc[stack] != null)
+                                                smc[stack].Add(p);
+                                        }
+                                        reader.MoveToElement();
+                                    }
+                                    else
+                                        readerName[stack] = reader.Name;
+                                }
+                                break;
+                            case XmlNodeType.Text:
+                                if (stack >= 0 && stack < 100)
+                                {
+                                    if (smc[stack] != null)
+                                    {
+                                        string n = smc[stack].idShort;
+                                        string v = reader.Value;
+                                        var p = AdminShell.Property.CreateNew(n);
+                                        p.TimeStampCreate = timeStamp;
+                                        p.setTimeStamp(timeStamp);
+                                        p.valueType = "string";
+                                        p.value = v;
+                                        smc[stack].Add(p);
+                                    }
+                                    else
+                                        readerValue[stack] = reader.Value;
+                                }
+                                break;
+                            case XmlNodeType.EndElement:
+                                if (stack >= 0 && stack < 100)
+                                {
+                                    if (readerName[stack] != null && readerValue[stack] != null)
+                                    {
+                                        var pxml = AdminShell.Property.CreateNew(readerName[stack]);
+                                        pxml.TimeStampCreate = timeStamp;
+                                        pxml.setTimeStamp(timeStamp);
+                                        pxml.valueType = "string";
+                                        pxml.value = readerValue[stack];
+                                        if (stack == 0)
+                                        {
+                                            if (c != null)
+                                                c.Add(pxml);
+                                        }
+                                        if (stack > 0)
+                                        {
+                                            if (smc[stack - 1] != null)
+                                                smc[stack - 1].Add(pxml);
+                                        }
+                                    }
+                                    readerName[stack] = null;
+                                    readerValue[stack] = null;
+                                    smc[stack] = null;
+                                }
+                                stack--;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+
+            return;
         }
     }
 }
