9ff355a8c0b8f81e511b737bba61289821752663
Andreas Orzelski <aorzelski@phoenixcontact.com>
2022-08-15 15:32:56 +0200

    Changes for Linux segmentation fault

--- src/AasxServerStandardBib/Program.cs
+++ src/AasxServerStandardBib/Program.cs
@@ -533,6 +533,7 @@ namespace AasxServer
             // MICHA MICHA
             AasxTimeSeries.TimeSeries.timeSeriesInit();
 
+            /* OZOZ
             var _energyModelInstances = new List<EnergyModelInstance>();
             foreach (var penv in AasxServer.Program.env)
             {
@@ -541,13 +542,15 @@ namespace AasxServer
                     EnergyModelInstance.FindAllSmInstances(penv?.AasEnv));
             }
             EnergyModelInstance.StartAllAsOneThread(_energyModelInstances);
+            */
 
             AasxTask.taskInit();
 
             RunScript(true);
             //// Initialize            NewDataAvailable?.Invoke(null, EventArgs.Empty);
 
-            ProductChange.pcn.pcnInit();
+            // Disable, because of Linux Segementation Fault
+            //// ProductChange.pcn.pcnInit();
 
             isLoading = false;
 
