f91dfb098c3a36b8ce9286021c2618d58ebc55aa
aorzelskiGH <aorzelski@phoenixcontact.com>
2023-06-12 08:04:22 +0200

    Copy db migrations

---
+++ src/AasxServerStandardBib/Migrations/20230605092838_resortTables.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AasxServerStandardBib.Migrations
+{
+    /// <inheritdoc />
+    public partial class resortTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FloatValueCount",
+                table: "DbConfigSets");
+
+            migrationBuilder.DropColumn(
+                name: "IntValueCount",
+                table: "DbConfigSets");
+
+            migrationBuilder.DropColumn(
+                name: "StringValueCount",
+                table: "DbConfigSets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FloatValueCount",
+                table: "DbConfigSets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "IntValueCount",
+                table: "DbConfigSets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StringValueCount",
+                table: "DbConfigSets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
