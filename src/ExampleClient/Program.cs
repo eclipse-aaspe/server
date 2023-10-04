1b83459a23b02d2d06716eda9b85740b8cbf1f68
aorzelskiGH <aorzelski@phoenixcontact.com>
2023-10-04 10:21:40 +0200

    Add first example client

---
+++ src/ExampleClient/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Text.Json.Nodes;
+using System.Text;
+using AasCore.Aas3_0;
+using static System.Net.WebRequestMethods;
+using Microsoft.IdentityModel.Tokens;
+
+Console.WriteLine("AAS Example Client V3");
+Console.WriteLine();
+
+// Find full API at https://v3.admin-shell-io.com/swagger
+// GET shells, which is with pagenation
+Console.WriteLine("GET https://v3.admin-shell-io.com/shells");
+
+string requestPath = "https://v3.admin-shell-io.com/shells";
+
+var handler = new HttpClientHandler();
+
+if (!requestPath.Contains("localhost"))
+{
+    handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
+}
+
+var client = new HttpClient(handler);
+
+bool error = false;
+HttpResponseMessage response = new HttpResponseMessage();
+
+var task = Task.Run(async () =>
+{
+    response = await client.GetAsync(requestPath);
+});
+task.Wait();
+var json = response.Content.ReadAsStringAsync().Result;
+if (!string.IsNullOrEmpty(json))
+{
+    MemoryStream mStrm = new MemoryStream(Encoding.UTF8.GetBytes(json));
+    JsonNode node = System.Text.Json.JsonSerializer.DeserializeAsync<JsonNode>(mStrm).Result;
+    if (node is JsonObject jo)
+    {
+        if (jo.ContainsKey("result"))
+        {
+            node = (JsonNode)jo["result"];
+            if (node is JsonArray a)
+            {
+                // iterate shells
+                foreach (JsonNode n in a)
+                {
+                    if (n != null)
+                    {
+                        try
+                        {
+                            // Deserialize shell
+                            var aas = Jsonization.Deserialize.AssetAdministrationShellFrom(n);
+                            Console.WriteLine("Received AAS: " + aas.IdShort);
+
+                            // Iterate submodels
+                            if (aas.Submodels != null && aas.Submodels.Count > 0)
+                            {
+                                foreach (var smr in aas.Submodels)
+                                {
+                                    requestPath = "https://v3.admin-shell-io.com/submodels/" + Base64UrlEncoder.Encode(smr.Keys[0].Value);
+                                    Console.WriteLine("GET " + requestPath);
+
+                                    task = Task.Run(async () =>
+                                    {
+                                        response = await client.GetAsync(requestPath);
+                                    });
+                                    task.Wait();
+                                    json = response.Content.ReadAsStringAsync().Result;
+                                    if (!string.IsNullOrEmpty(json))
+                                    {
+                                        var mStrm2 = new MemoryStream(Encoding.UTF8.GetBytes(json));
+                                        var node2 = System.Text.Json.JsonSerializer.DeserializeAsync<JsonNode>(mStrm2).Result;
+                                        var submodel = Jsonization.Deserialize.SubmodelFrom(node2);
+                                        Console.WriteLine("Received Submodel: " + submodel.IdShort);
+                                        // Iterate submodel here
+                                        // See VisitorThrough in AasCore
+                                        // See VisitorAASX in entityFW.cs
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            string r = "ERROR GET; " + response.StatusCode.ToString();
+                            r += " ; " + requestPath;
+                            if (response.Content != null)
+                                r += " ; " + response.Content.ReadAsStringAsync().Result;
+                            Console.WriteLine(r);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
