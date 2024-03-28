a54e6d5458917eaab486574d8e7c944f0801502a
Freezor <oliver.fries@iosb-ina.fraunhofer.de>
2024-03-28 10:42:02 +0100

    fix simple issues

--- src/AasxServerBlazor/TreeVisualisation/QrCodeService.cs
+++ src/AasxServerBlazor/TreeVisualisation/QrCodeService.cs
@@ -14,47 +14,38 @@ public class QrCodeService
             return "";
         }
 
-        var prefix = "http://";
-        var hostPort = Program.hostPort;
+        Program.hostPort.Split(':');
 
-        var split = Program.hostPort.Split(':');
+        var treeItemTag = treeItem.Tag;
 
-        var o = treeItem.Tag;
-
-        if (o is AssetAdministrationShell)
-        {
-            var aas = o as AssetAdministrationShell;
-            var asset = aas.AssetInformation;
-            if (asset != null)
-            {
-                var url = asset.GlobalAssetId;
-                var link = url;
-
-                return link;
-            }
-        }
-
-        return "";
-    }
-
-    public string GetQrCodeImage(TreeItem treeItem)
-    {
-        if (treeItem == null)
+        if (treeItemTag is not AssetAdministrationShell aas)
         {
             return "";
         }
 
-        var o = treeItem.Tag;
-
-        if (o is AssetAdministrationShell)
+        var asset = aas.AssetInformation;
+        if (asset == null)
         {
-            var image = Program.generatedQrCodes[treeItem.Tag];
-
-            if (image != null)
-                return image;
+            return "";
         }
 
-        return "";
+        var url = asset.GlobalAssetId;
+
+        return url;
+    }
+
+    public string GetQrCodeImage(TreeItem treeItem)
+    {
+        var o = treeItem?.Tag;
+
+        if (o is not AssetAdministrationShell)
+        {
+            return "";
+        }
+
+        var image = Program.generatedQrCodes[treeItem.Tag];
+
+        return image ?? "";
     }
 
     public void CreateQrCodeImage(TreeItem treeItem)
@@ -64,40 +55,37 @@ public class QrCodeService
             return;
         }
 
-        var prefix = "http://";
-        var hostPort = Program.hostPort;
+        Program.hostPort.Split(':');
 
-        var split = Program.hostPort.Split(':');
+        var treeItemTag = treeItem.Tag;
 
-        var o = treeItem.Tag;
-
-        if (o is AssetAdministrationShell)
+        if (treeItemTag is not AssetAdministrationShell aas)
         {
-            if (Program.generatedQrCodes.ContainsKey(treeItem.Tag))
-            {
-                Program.generatedQrCodes.Remove(treeItem.Tag);
-                return;
-            }
-
-            var aas = o as AssetAdministrationShell;
-            var asset = aas.AssetInformation;
-            if (asset != null)
-            {
-                var url = asset.GlobalAssetId;
-                var link = url;
-
-                var qrGenerator = new QRCodeGenerator();
-                var qrCodeData = qrGenerator.CreateQrCode(link, QRCodeGenerator.ECCLevel.Q);
-                var qrCode = new QRCode(qrCodeData);
-                var qrCodeImage = qrCode.GetGraphic(20);
-                using (MemoryStream memory = new MemoryStream())
-                {
-                    qrCodeImage.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                    var base64 = Convert.ToBase64String(memory.ToArray());
-
-                    Program.generatedQrCodes.Add(treeItem.Tag, base64);
-                }
-            }
+            return;
         }
+
+        if (Program.generatedQrCodes.ContainsKey(treeItem.Tag))
+        {
+            Program.generatedQrCodes.Remove(treeItem.Tag);
+            return;
+        }
+
+        var asset = aas.AssetInformation;
+        if (asset == null)
+        {
+            return;
+        }
+
+        var url = asset.GlobalAssetId;
+
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+        var qrCode = new QRCode(qrCodeData);
+        var qrCodeImage = qrCode.GetGraphic(20);
+        using var memory = new MemoryStream();
+        qrCodeImage.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+        var base64 = Convert.ToBase64String(memory.ToArray());
+
+        Program.generatedQrCodes.Add(treeItem.Tag, base64);
     }
 }
