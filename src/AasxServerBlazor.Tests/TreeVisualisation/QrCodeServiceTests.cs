782b6882c152abe52e64b7ee86cfd8429c916f56
Freezor <oliver.fries@iosb-ina.fraunhofer.de>
2024-03-28 10:38:29 +0100

    Extract QrCodeService.cs from TreePage.razor and write tests for it.

---
+++ src/AasxServerBlazor.Tests/TreeVisualisation/QrCodeServiceTests.cs
@@ -0,0 +1,139 @@
+﻿using System.Drawing;
+using System.Reflection;
+using AasCore.Aas3_0;
+using AasxServer;
+using AasxServerBlazor.TreeVisualisation;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using QRCoder;
+
+namespace AasxServerBlazor.Tests.TreeVisualisation;
+
+public class QrCodeServiceTests
+{
+    private readonly Fixture _fixture;
+
+    public QrCodeServiceTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Customize(new AutoMoqCustomization());
+        _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    [Fact]
+    public void GetQrCodeLink_WithNullTreeItem_ReturnsEmptyString()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+
+        // Act
+        var result = qrCodeService.GetQrCodeLink(null);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetQrCodeLink_WithValidTreeItem_ReturnsLink()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+        var treeItem = _fixture.Create<TreeItem>();
+        var assetAdministrationShell = _fixture.Create<AssetAdministrationShell>();
+        var assetInformation = _fixture.Create<AssetInformation>();
+        assetInformation.GlobalAssetId = "http://example.com";
+        assetAdministrationShell.AssetInformation = assetInformation;
+        treeItem.Tag = assetAdministrationShell;
+
+        // Act
+        var result = qrCodeService.GetQrCodeLink(treeItem);
+
+        // Assert
+        result.Should().NotBeEmpty();
+        result.Should().Be("http://example.com");
+    }
+
+    [Fact]
+    public void GetQrCodeImage_WithNullTreeItem_ReturnsEmptyString()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+
+        // Act
+        var result = qrCodeService.GetQrCodeImage(null);
+
+        // Assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetQrCodeImage_WithValidTreeItem_ReturnsImage()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+        var treeItem = _fixture.Create<TreeItem>();
+        treeItem.Tag = _fixture.Create<AssetAdministrationShell>();
+        var expectedImage = "base64ImageString";
+        Program.generatedQrCodes.Add(treeItem.Tag, expectedImage);
+
+        // Act
+        var result = qrCodeService.GetQrCodeImage(treeItem);
+
+        // Assert
+        result.Should().NotBeEmpty();
+        result.Should().Be(expectedImage);
+    }
+
+    [Fact]
+    public void CreateQrCodeImage_WithNullTreeItem_DoesNotAddToGeneratedQrCodes()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+
+        // Act
+        qrCodeService.CreateQrCodeImage(null);
+
+        // Assert
+        Program.generatedQrCodes.Should().BeEmpty();
+    }
+
+    [Fact (Skip="We cannot let this test run at the moment, because the QRCodeGenerator is not mockable.")]
+    public void CreateQrCodeImage_ValidTreeItem_CreatesQrCodeImage()
+    {
+        // Arrange
+        var qrCodeService = new QrCodeService();
+
+        var assetAdministrationShell = _fixture.Create<AssetAdministrationShell>();
+        var assetInformation = _fixture.Create<AssetInformation>();
+        assetInformation.GlobalAssetId = "http://example.com";
+        assetAdministrationShell.AssetInformation = assetInformation;
+
+        var treeItem = new TreeItem
+        {
+            Tag = assetAdministrationShell
+        };
+        var qrCodeGeneratorMock = new Mock<QRCodeGenerator>();
+        var qrCodeMock = new Mock<QRCode>();
+        qrCodeGeneratorMock.Setup(x => x.CreateQrCode(It.IsAny<string>(), It.IsAny<QRCodeGenerator.ECCLevel>(), It.IsAny<bool>(), It.IsAny<bool>(),
+                It.IsAny<QRCodeGenerator.EciMode>(), It.IsAny<int>()))
+            .Returns(_fixture.Create<QRCodeData>());
+        qrCodeMock.Setup(x => x.GetGraphic(It.IsAny<int>())).Returns(new Bitmap(10, 10));
+        Program.generatedQrCodes = new System.Collections.Generic.Dictionary<object, string>();
+
+        // Act
+        qrCodeService.CreateQrCodeImage(treeItem);
+
+        // Assert
+        qrCodeGeneratorMock.Verify(x => x.CreateQrCode("http://example.com", QRCodeGenerator.ECCLevel.Q, It.IsAny<bool>(),
+            It.IsAny<bool>(),
+            It.IsAny<QRCodeGenerator.EciMode>(),
+            It.IsAny<int>()), Times.Once);
+        qrCodeMock.Verify(x => x.GetGraphic(20), Times.Once);
+        Assert.Single(Program.generatedQrCodes);
+        Assert.True(Program.generatedQrCodes.ContainsKey(treeItem.Tag));
+        Assert.NotEmpty(Program.generatedQrCodes[treeItem.Tag]);
+    }
+}
---
+++ src/AasxServerBlazor/TreeVisualisation/QrCodeService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using AasxServer;
+using QRCoder;
+
+namespace AasxServerBlazor.TreeVisualisation;
+
+public class QrCodeService
+{
+    public string GetQrCodeLink(TreeItem treeItem)
+    {
+        if (treeItem == null)
+        {
+            return "";
+        }
+
+        var prefix = "http://";
+        var hostPort = Program.hostPort;
+
+        var split = Program.hostPort.Split(':');
+
+        var o = treeItem.Tag;
+
+        if (o is AssetAdministrationShell)
+        {
+            var aas = o as AssetAdministrationShell;
+            var asset = aas.AssetInformation;
+            if (asset != null)
+            {
+                var url = asset.GlobalAssetId;
+                var link = url;
+
+                return link;
+            }
+        }
+
+        return "";
+    }
+
+    public string GetQrCodeImage(TreeItem treeItem)
+    {
+        if (treeItem == null)
+        {
+            return "";
+        }
+
+        var o = treeItem.Tag;
+
+        if (o is AssetAdministrationShell)
+        {
+            var image = Program.generatedQrCodes[treeItem.Tag];
+
+            if (image != null)
+                return image;
+        }
+
+        return "";
+    }
+
+    public void CreateQrCodeImage(TreeItem treeItem)
+    {
+        if (treeItem == null)
+        {
+            return;
+        }
+
+        var prefix = "http://";
+        var hostPort = Program.hostPort;
+
+        var split = Program.hostPort.Split(':');
+
+        var o = treeItem.Tag;
+
+        if (o is AssetAdministrationShell)
+        {
+            if (Program.generatedQrCodes.ContainsKey(treeItem.Tag))
+            {
+                Program.generatedQrCodes.Remove(treeItem.Tag);
+                return;
+            }
+
+            var aas = o as AssetAdministrationShell;
+            var asset = aas.AssetInformation;
+            if (asset != null)
+            {
+                var url = asset.GlobalAssetId;
+                var link = url;
+
+                var qrGenerator = new QRCodeGenerator();
+                var qrCodeData = qrGenerator.CreateQrCode(link, QRCodeGenerator.ECCLevel.Q);
+                var qrCode = new QRCode(qrCodeData);
+                var qrCodeImage = qrCode.GetGraphic(20);
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    qrCodeImage.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    var base64 = Convert.ToBase64String(memory.ToArray());
+
+                    Program.generatedQrCodes.Add(treeItem.Tag, base64);
+                }
+            }
+        }
+    }
+}
