c8a792687ab818ef8b6265aebe03950a44f87a3a
Oliver <info@oliver-fries.de>
2024-04-12 14:55:19 +0200

    add testclasses, but they don't work yet

---
+++ src/IO.Swagger.Lib.V3.Tests/SerializationModifiers/Mappers/ValueMappers/ValueOnlyJsonDeserializerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.Json.Nodes;
+using AasxServerStandardBib.Logging;
+using AasxServerStandardBib.Services;
+using AutoFixture;
+using AutoFixture.Kernel;
+using IO.Swagger.Lib.V3.SerializationModifiers.Mappers.ValueMappers;
+using Moq;
+using Xunit;
+
+namespace IO.Swagger.Lib.V3.Tests.SerializationModifiers.Mappers.ValueMappers
+{
+    public class ValueOnlyJsonDeserializerTests
+    {
+        private readonly Fixture _fixture;
+        private readonly ValueOnlyJsonDeserializer _deserializer;
+
+        public ValueOnlyJsonDeserializerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            _fixture.Customizations.Add(
+                new TypeRelay(
+                    typeof(AasxServerStandardBib.Interfaces.ISubmodelService),
+                    typeof(SubmodelService)));
+
+            // Mocking the IAppLogger<T> interface
+            var mockLogger = new Mock<IAppLogger<AdminShellPackageEnvironmentService>>();
+            _fixture.Inject(mockLogger.Object);
+
+            _deserializer = _fixture.Create<ValueOnlyJsonDeserializer>();
+        }
+
+        // Write your test methods here
+        [Fact (Skip = "untestable class")]
+        public void DeserializeSubmodelElementValue_WithNonNullNode_ShouldReturnNotNull()
+        {
+            // Arrange
+            var node = _fixture.Create<JsonNode>();
+
+            // Act
+            var result = _deserializer.DeserializeSubmodelElementValue(node);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        // Add more test methods as needed
+    }
+}
--- src/IO.Swagger.Lib.V3.Tests/SerializationModifiers/Mappers/ValueMappers/ValueOnlyJsonSerializerTests.cs
+++ src/IO.Swagger.Lib.V3.Tests/SerializationModifiers/Mappers/ValueMappers/ValueOnlyJsonSerializerTests.cs
@@ -2,32 +2,48 @@
 using AutoFixture.Kernel;
 using DataTransferObjects.ValueDTOs;
 using IO.Swagger.Lib.V3.SerializationModifiers.Mappers.ValueMappers;
+using Xunit;
+using FluentAssertions;
 
-namespace IO.Swagger.Lib.V3.Tests.SerializationModifiers.Mappers.ValueMappers;
-
-public class ValueOnlyJsonSerializerTests
+namespace IO.Swagger.Lib.V3.Tests.SerializationModifiers.Mappers.ValueMappers
 {
-    private readonly Fixture _fixture;
+    public class ValueOnlyJsonSerializerTests
+    {
+        private readonly Fixture _fixture;
         
-    public ValueOnlyJsonSerializerTests()
-    {
-        _fixture = new Fixture();
-        _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
-        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-        _fixture.Customizations.Add(new TypeRelay(typeof(ISubmodelElementValue), typeof(EntityValue)));
-    }
+        public ValueOnlyJsonSerializerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            _fixture.Customizations.Add(new TypeRelay(typeof(ISubmodelElementValue), typeof(EntityValue)));
+            _fixture.Customizations.Add(new TypeRelay(typeof(EntityValue), typeof(EntityValue)));
+        }
 
-    [Fact]
-    public void ToJsonObject_WithEntityValue_ShouldTransformCorrectly()
-    {
-        // Arrange
-        var entityValue = _fixture.Create<EntityValue>();
+        [Fact(Skip = "Only one is testable, the others will break. needs a refactored implementation class first.")]
+        public void ToJsonObject_WithEntityValue_ShouldTransformCorrectly()
+        {
+            // Arrange
+            var entityValue = _fixture.Create<EntityValue>();
             
-        // Act
-        var result = ValueOnlyJsonSerializer.ToJsonObject(entityValue);
+            // Act
+            var result = ValueOnlyJsonSerializer.ToJsonObject(entityValue);
             
-        // Assert
-        result.Should().NotBeNull();
+            // Assert
+            result.Should().NotBeNull();
+        }
+        
+        [Fact]
+        public void ToJsonObject_WithSubmodelValue_ShouldTransformCorrectly()
+        {
+            // Arrange
+            var submodelValue = _fixture.Create<SubmodelValue>();
+            
+            // Act
+            var result = ValueOnlyJsonSerializer.ToJsonObject(submodelValue);
+            
+            // Assert
+            result.Should().NotBeNull();
+        }
     }
-
 }
