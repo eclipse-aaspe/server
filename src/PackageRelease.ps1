8588c0b300d441b51cf1deead13228ef97dbeaf4
Oliver <info@oliver-fries.de>
2024-06-10 16:59:05 +0200

    clean up pipeline scripts and allow usage of semantic versioning (#268)
    Update the process for creating versions:
    
    Update the Changelog
    Move all current changes to the [Released] section in the changelog.
    Determine the new version number based on semantic versioning and add the release date.
    Update the Version Configuration
    Use the new version number to update the current_version.cfg.
    Push Changes
    Push these changes to the branch you want to release from (main/develop/or your branch).
    Create a New Version Tag on GitHub
    Go to GitHub and create a new version tag on the branch. This tag should be the full version string, such as "0.3.0-1-aasV3-alpha-develop". The build number can be omitted in the tag name.

--- src/PackageRelease.ps1
+++ src/PackageRelease.ps1
@@ -1,7 +1,7 @@
 param(
-    [Parameter(HelpMessage = "Suffix to be appended to the version (e.g., alpha, beta)", Mandatory = $false)]
+    [Parameter(HelpMessage = "Suffix to be appended to the version (e.g., alpha, beta)", Mandatory = $true)]
     [string]
-    $suffix = "alpha"
+    $version = "0.3.0"
 )
 
 # Set error action preference to stop on errors.
@@ -9,77 +9,6 @@ $ErrorActionPreference = "Stop"
 
 # Import necessary functions from Common.psm1 module.
 Import-Module (Join-Path $PSScriptRoot Common.psm1) -Function GetArtefactsDir
-
-function GetNextBuildNumber {
-    # Read the current build number from the file.
-    $currentBuild = Get-Content (Join-Path $PSScriptRoot "current_build_number.cfg") | ForEach-Object { $_.Trim() }
-
-    # Increment the build number and save it back to the file.
-    $nextBuild = [int]$currentBuild + 1
-    $nextBuild | Set-Content (Join-Path $PSScriptRoot "current_build_number.cfg")
-
-    # Return the incremented build number.
-    return $nextBuild
-}
-
-function GetVersionCore {
-    # Read the current version from the file.
-    $versionCore = Get-Content (Join-Path $PSScriptRoot "current_version.cfg") | ForEach-Object { $_.Trim() }
-
-    # Return the version core.
-    return $versionCore
-}
-
-function GetBuildSuffix {
-    # Determine if the build is on the main or release branch.
-    $branch = git branch --show-current 2>$null  # Suppress errors if not in a Git repository
-    if (-not $?) {
-        Write-Host "Not in a Git repository. Assuming develop branch."
-        return "develop"
-    }
-
-    if ($branch -eq "main") {
-        return "latest"
-    }
-    elseif ($branch -eq "release") {
-        return "stable"
-    }
-    else {
-        Write-Host "Not main or release branch. Assuming develop branch."
-        return "develop"
-    }
-}
-
-function GetVersion {
-    # Get the version core from the file.
-    $versionCore = GetVersionCore
-
-    # Get the build suffix based on the branch.
-    $buildSuffix = GetBuildSuffix
-
-    # Get the next build number.
-    $buildNumber = GetNextBuildNumber
-    
-    $aasmodel = "aasV3"
-
-    # Construct the semantic version.
-    $semanticVersion = "$versionCore-$buildNumber-$aasmodel-$suffix-$buildSuffix"
-
-    return $semanticVersion
-}
-
-function UpdateProjectVersions($version) {
-    # Get all csproj files in the solution.
-    $projectFiles = Get-ChildItem -Path "$PSScriptRoot\.." -Recurse -Filter *.csproj
-
-    # Iterate through each project file and update the <Version> tag.
-    foreach ($file in $projectFiles) {
-        Write-Host "Updating version in $($file.FullName)"
-        (Get-Content -Path $file.FullName) | ForEach-Object {
-            $_ -replace '<Version>.*<\/Version>', "<Version>$version</Version>"
-        } | Set-Content -Path $file.FullName
-    }
-}
 
 function PackageRelease($outputDir, $version) {
     # Define the base directory where built release files are stored.
@@ -118,16 +47,10 @@ function PackageRelease($outputDir, $version) {
 }
 
 function Main {
-    # Get the semantic version.
-    $version = GetVersion
-
     # Validate the generated semantic version.
     if ($version -eq "") {
         throw "Failed to generate semantic version."
     }
-
-    # Update project versions.
-    UpdateProjectVersions $version
 
     # Define the output directory for the packaged release.
     $outputDir = Join-Path $(GetArtefactsDir) "release" | Join-Path -ChildPath $version
