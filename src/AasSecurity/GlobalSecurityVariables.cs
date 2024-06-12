6a0daf66b6025ee3a00801f5ee4bf610be6dbd9a
Oliver <info@oliver-fries.de>
2024-06-12 16:23:37 +0200

    Freezor/refactor program env from array to enumerable (#2)
    * remove old code
    
    * fix call of now deleted code
    
    * Remove all references to OPCUA
    
    * update CHANGELOG.md
    
    * transform program.env from array to List
    
    * Fix indexOf from array to list
    
    * fix launch and prevent certificate crash
    
    * use target-type new

--- src/AasSecurity/GlobalSecurityVariables.cs
+++ src/AasSecurity/GlobalSecurityVariables.cs
@@ -1,15 +1,15 @@
-﻿using AasSecurity.Models;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography.X509Certificates;
+using AasSecurity.Models;
 
 namespace AasSecurity
 {
     public static class GlobalSecurityVariables
     {
         public static bool WithAuthentication { get; set; }
-        internal static List<SecurityRole> SecurityRoles = new List<SecurityRole>();
-        internal static List<X509Certificate2> ServerCertificates = new List<X509Certificate2>();
-        internal static List<string> ServerCertFileNames = new List<string>();
-        internal static List<SecurityRight> SecurityRights = new List<SecurityRight>();
-        internal static Dictionary<string, string> SecurityUsernamePassword = new Dictionary<string, string>();
+        internal static List<SecurityRole> SecurityRoles = new();
+        internal static List<X509Certificate2> ServerCertificates = new();
+        internal static List<string> ServerCertFileNames = new();
+        internal static List<SecurityRight> SecurityRights = new();
+        internal static Dictionary<string, string> SecurityUsernamePassword = new();
     }
 }
