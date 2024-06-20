87b5323684cbf67de36c0fcba0023ea1d248eb45
Oliver <info@oliver-fries.de>
2024-06-20 22:43:09 +0200

    Create unit test project and add it into process to run in every build (#290)
    * add test structur and workflow
    
    * change build step
    
    * add working directories
    
    * improve release check
    
    * update permissions
    
    * update permissions
    
    * update permissions
    
    * update permissions
    
    * update test report
    
    * fix path
    
    * try other action
    
    * tryou
    
    * revert
    
    * update test report
    
    * fix wrong step
    
    * update working directory
    
    * update test workflow
    
    * remove testreport
    
    * rename
    
    * verify
    
    * update naming
    
    * readd everythin

---
+++ src/AasSecurityTests/AasSecurityContextTest.cs
@@ -0,0 +1,103 @@
+using AasSecurity.Models;
+
+namespace AasSecurity;
+
+using Exceptions;
+
+public class AasSecurityContextTests
+{
+    private readonly IFixture _fixture;
+
+    public AasSecurityContextTests()
+    {
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+    }
+
+    [Theory]
+    [InlineData("post", AccessRights.CREATE)]
+    [InlineData("head", AccessRights.READ)]
+    [InlineData("get", AccessRights.READ)]
+    [InlineData("put", AccessRights.UPDATE)]
+    [InlineData("delete", AccessRights.DELETE)]
+    [InlineData("patch", AccessRights.UPDATE)]
+    public void Constructor_Should_SetNeededRights_Correctly(string httpOperation, AccessRights expectedRights)
+    {
+        // Arrange
+        var accessRole = _fixture.Create<string>();
+        var route      = _fixture.Create<string>();
+
+        // Act
+        var context = new AasSecurityContext(accessRole, route, httpOperation);
+
+        // Assert
+        context.NeededRights.Should().Be(expectedRights);
+    }
+
+    [Theory]
+    [InlineData("unsupported")]
+    [InlineData(" ")]
+    [InlineData("")]
+    public void Constructor_Should_ThrowAuthorizationException_ForUnsupportedHttpOperation(string unsupportedHttpOperation)
+    {
+        // Arrange
+        var accessRole = _fixture.Create<string>();
+        var route      = _fixture.Create<string>();
+
+        // Act
+        Action act = () => new AasSecurityContext(accessRole, route, unsupportedHttpOperation);
+
+        // Assert
+        act.Should().Throw<AuthorizationException>().WithMessage($"Unsupported HTTP Operation {unsupportedHttpOperation}");
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    public void Constructor_Should_ThrowNullReferenceException_ForUnsupportedHttpOperation(string unsupportedHttpOperation)
+    {
+        // Arrange
+        var accessRole = _fixture.Create<string>();
+        var route      = _fixture.Create<string>();
+
+        // Act
+        Action act = () => new AasSecurityContext(accessRole, route, unsupportedHttpOperation);
+
+        // Assert
+        act.Should().Throw<NullReferenceException>();
+    }
+
+    [Fact]
+    public void Constructor_Should_SetAccessRoleAndRoute_Correctly()
+    {
+        // Arrange
+        var accessRole    = _fixture.Create<string>();
+        var route         = _fixture.Create<string>();
+        var httpOperation = "get";
+
+        // Act
+        var context = new AasSecurityContext(accessRole, route, httpOperation);
+
+        // Assert
+        context.AccessRole.Should().Be(accessRole);
+        context.Route.Should().Be(route);
+    }
+
+    [Theory]
+    [InlineData("PoSt", AccessRights.CREATE)]
+    [InlineData("hEaD", AccessRights.READ)]
+    [InlineData("gEt", AccessRights.READ)]
+    [InlineData("pUt", AccessRights.UPDATE)]
+    [InlineData("dEleTe", AccessRights.DELETE)]
+    [InlineData("PaTcH", AccessRights.UPDATE)]
+    public void Constructor_Should_HandleCaseInsensitiveHttpOperations(string httpOperation, AccessRights expectedRights)
+    {
+        // Arrange
+        var accessRole = _fixture.Create<string>();
+        var route      = _fixture.Create<string>();
+
+        // Act
+        var context = new AasSecurityContext(accessRole, route, httpOperation);
+
+        // Assert
+        context.NeededRights.Should().Be(expectedRights);
+    }
+}
