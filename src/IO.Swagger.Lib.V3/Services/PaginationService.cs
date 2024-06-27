54a1b86013b2d6a6eeb2151a882f2141a8ac22a9
Oliver Fries <info@oliver-fries.de>
2024-06-27 10:10:38 +0200

    Refactor PaginationService.cs

--- src/IO.Swagger.Lib.V3/Services/PaginationService.cs
+++ src/IO.Swagger.Lib.V3/Services/PaginationService.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 
 namespace IO.Swagger.Lib.V3.Services;
 
+using System.Linq;
+
 /// <inheritdoc />
 public class PaginationService : IPaginationService
 {
@@ -17,46 +19,22 @@ public class PaginationService : IPaginationService
     /// Constructor for PaginationService.
     /// </summary>
     /// <param name="logger">Logger instance for logging.</param>
-    public PaginationService(IAppLogger<PaginationService> logger)
-    {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
+    public PaginationService(IAppLogger<PaginationService> logger) => _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
     /// <inheritdoc />
     public PagedResult GetPaginatedList<T>(List<T> sourceList, PaginationParameters paginationParameters)
     {
-        var outputList = new List<T>();
         var startIndex = paginationParameters.Cursor;
         var endIndex   = startIndex + paginationParameters.Limit - 1;
 
-        //cap the endIndex
-        if (endIndex > sourceList.Count - 1)
-        {
-            endIndex = sourceList.Count - 1;
-        }
+        CapEndIndex(sourceList, ref endIndex);
 
-        //If there are fewer elements in the sourceList than "from"
-        if (startIndex > sourceList.Count - 1)
-        {
-            _logger.LogError($"There are less elements in the retrieved list than requested pagination - (from: {startIndex}, size:{endIndex})");
-        }
+        LogErrorIfStartIndexOutOfBounds(sourceList, startIndex, endIndex);
 
-        for (var i = startIndex; i <= endIndex; i++)
-        {
-            outputList.Add(sourceList[i]);
-        }
+        var outputList     = GetPaginationList(sourceList, startIndex, endIndex);
+        var pagingMetadata = CreatePagingMetadata(sourceList, endIndex);
 
-        //Creating pagination result
-        var pagingMetadata = new PagedResultPagingMetadata();
-        if (endIndex < sourceList.Count - 1)
-        {
-            pagingMetadata.cursor = Convert.ToString(endIndex + 1);
-        }
-
-        var paginationResult = new PagedResult {result = outputList.ConvertAll(r => r as IClass), paging_metadata = pagingMetadata};
-
-        //return paginationResult;
-        return paginationResult;
+        return new PagedResult {result = outputList.ConvertAll(r => r as IClass), paging_metadata = pagingMetadata};
     }
 
     /// <inheritdoc />
@@ -64,50 +42,43 @@ public class PaginationService : IPaginationService
     {
         var startIndex = paginationParameters.Cursor;
         var endIndex   = startIndex + paginationParameters.Limit - 1;
-        var outputList = GetPaginationList(sourceList, startIndex, endIndex);
 
-        //Creating pagination result
+        CapEndIndex(sourceList, ref endIndex);
+
+        LogErrorIfStartIndexOutOfBounds(sourceList, startIndex, endIndex);
+
+        var outputList     = GetPaginationList(sourceList, startIndex, endIndex);
+        var pagingMetadata = CreatePagingMetadata(sourceList, endIndex);
+
+        return new PackageDescriptionPagedResult {result = outputList, paging_metadata = pagingMetadata};
+    }
+
+    private static void CapEndIndex<T>(List<T> sourceList, ref int endIndex)
+    {
+        if (endIndex > sourceList.Count - 1)
+        {
+            endIndex = sourceList.Count - 1;
+        }
+    }
+
+    private void LogErrorIfStartIndexOutOfBounds<T>(List<T> sourceList, int startIndex, int endIndex)
+    {
+        if (startIndex > sourceList.Count - 1)
+        {
+            _logger.LogError($"There are fewer elements in the retrieved list than requested for pagination - (from: {startIndex}, size: {endIndex})");
+        }
+    }
+
+    private static List<T> GetPaginationList<T>(List<T> sourceList, int startIndex, int endIndex) => sourceList.Skip(startIndex).Take(endIndex - startIndex + 1).ToList();
+
+    private static PagedResultPagingMetadata CreatePagingMetadata<T>(List<T> sourceList, int endIndex)
+    {
         var pagingMetadata = new PagedResultPagingMetadata();
         if (endIndex < sourceList.Count - 1)
         {
             pagingMetadata.cursor = Convert.ToString(endIndex + 1);
         }
 
-        var paginationResult = new PackageDescriptionPagedResult {result = outputList, paging_metadata = pagingMetadata};
-
-        //return paginationResult;
-        return paginationResult;
-    }
-
-    /// <summary>
-    /// Retrieves a paginated subset of a generic list based on provided indices.
-    /// </summary>
-    /// <typeparam name="T">Type of items in the list.</typeparam>
-    /// <param name="sourceList">Source list to paginate.</param>
-    /// <param name="startIndex">Starting index of the subset.</param>
-    /// <param name="endIndex">Ending index of the subset.</param>
-    /// <returns>Paginated subset of the source list.</returns>
-    private List<T> GetPaginationList<T>(List<T> sourceList, int startIndex, int endIndex)
-    {
-        var outputList = new List<T>();
-
-        //cap the endIndex
-        if (endIndex > sourceList.Count - 1)
-        {
-            endIndex = sourceList.Count - 1;
-        }
-
-        //If there are fewer elements in the sourceList than "from"
-        if (startIndex > sourceList.Count - 1)
-        {
-            _logger.LogError($"There are less elements in the retried list than requested pagination - (from: {startIndex}, size:{endIndex})");
-        }
-
-        for (var i = startIndex; i <= endIndex; i++)
-        {
-            outputList.Add(sourceList[i]);
-        }
-
-        return outputList;
+        return pagingMetadata;
     }
 }
