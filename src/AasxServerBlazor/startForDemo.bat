3f37a7ee014ec1408359df4729556577efeaa561
martaf <marta.fullen@iosb-ina.fraunhofer.de>
2024-07-01 09:58:31 +0200

    .bat (experimental)

--- src/AasxServerBlazor/startForDemo.bat
+++ src/AasxServerBlazor/startForDemo.bat
@@ -1,1 +1,15 @@
+@echo off
+REM *******************************************************************************
+REM * Copyright (c) {2024} Contributors to the Eclipse Foundation
+REM *
+REM * See the NOTICE file(s) distributed with this work for additional
+REM * information regarding copyright ownership.
+REM *
+REM * This program and the accompanying materials are made available under the
+REM * terms of the Apache License Version 2.0 which is available at
+REM * https://www.apache.org/licenses/LICENSE-2.0
+REM *
+REM * SPDX-License-Identifier: Apache-2.0
+REM *******************************************************************************
+
 AasxServerBlazor.exe --no-security --data-path aasxs --external-blazor http://localhost:5001
