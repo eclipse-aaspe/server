48f0b00dfbc1142f8c35c3b82e167170f33ba88d
martaf <marta.fullen@iosb-ina.fraunhofer.de>
2024-07-01 09:58:08 +0200

    .cs

--- src/ExampleClient/Program.cs
+++ src/ExampleClient/Program.cs
@@ -1,4 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+/********************************************************************************
+* Copyright (c) {2024} Contributors to the Eclipse Foundation
+*
+* See the NOTICE file(s) distributed with this work for additional
+* information regarding copyright ownership.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Apache License Version 2.0 which is available at
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* SPDX-License-Identifier: Apache-2.0
+********************************************************************************/
+
+// See https://aka.ms/new-console-template for more information
 using AasCore.Aas3_0;
 using Microsoft.IdentityModel.Tokens;
 using System.Net;
