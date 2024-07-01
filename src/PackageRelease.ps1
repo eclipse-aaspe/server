0e3aa3edaee00e4d6ea7b446fb48dc8c7d7d7035
martaf <marta.fullen@iosb-ina.fraunhofer.de>
2024-07-01 09:53:08 +0200

    .ps1

--- src/PackageRelease.ps1
+++ src/PackageRelease.ps1
@@ -1,3 +1,16 @@
+<#*******************************************************************************
+* Copyright (c) {2024} Contributors to the Eclipse Foundation
+*
+* See the NOTICE file(s) distributed with this work for additional
+* information regarding copyright ownership.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Apache License Version 2.0 which is available at
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************#>
+
 param(
     [Parameter(HelpMessage = "Suffix to be appended to the version (e.g., alpha, beta)", Mandatory = $true)]
     [string]
