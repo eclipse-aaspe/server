73a03e70a4629cbb0c3044bbf1c61d6c236a8c24
Oliver Fries <info@oliver-fries.de>
2024-07-02 10:58:17 +0200

    update documentation for Logging interfaces and classes

--- src/AasxServerStandardBib/Logging/LoggerAdapter.cs
+++ src/AasxServerStandardBib/Logging/LoggerAdapter.cs
@@ -1,22 +1,55 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 
-namespace AasxServerStandardBib.Logging
+namespace AasxServerStandardBib.Logging;
+
+using System.Globalization;
+
+/// <summary>
+/// Adapter class that implements <see cref="IAppLogger{T}"/> for logging messages using Microsoft.Extensions.Logging.
+/// </summary>
+/// <typeparam name="T">The type of the class for which the logger is being created.</typeparam>
+public class LoggerAdapter<T> : IAppLogger<T>
 {
-    public class LoggerAdapter<T> : IAppLogger<T>
+    private readonly ILogger<T> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoggerAdapter{T}"/> class.
+    /// </summary>
+    /// <param name="loggerFactory">The factory for creating ILogger instances.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="loggerFactory"/> is null.</exception>
+    public LoggerAdapter(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger<T> _logger;
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+        _logger                          = loggerFactory.CreateLogger<T>();
+        ApplicationLogging.LoggerFactory = loggerFactory;
+    }
 
-        public LoggerAdapter(ILoggerFactory loggerFactory)
-        {
-            ArgumentNullException.ThrowIfNull(loggerFactory);
-            _logger = loggerFactory.CreateLogger<T>();
-            ApplicationLogging.LoggerFactory = loggerFactory;
-        }
+    /// <inheritdoc />
+    public void LogDebug(string message, params object[] args)
+    {
+        var formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
+        LogMessages.LogDebugMessage(_logger, formattedMessage);
+    }
 
-        public void LogDebug(string message, params object[] args) => _logger.LogDebug(message, args);
-        public void LogError(string message, params object[] args) => _logger.LogError(message, args);
-        public void LogInformation(string message, params object[] args) => _logger.LogInformation(message, args);
-        public void LogWarning(string message, params object[] args) => _logger.LogWarning(message, args);
+    /// <inheritdoc />
+    public void LogError(string message, params object[] args)
+    {
+        var formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
+        LogMessages.LogErrorMessage(_logger, formattedMessage);
+    }
+
+    /// <inheritdoc />
+    public void LogInformation(string message, params object[] args)
+    {
+        var formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
+        LogMessages.LogInformationMessage(_logger, formattedMessage);
+    }
+
+    /// <inheritdoc />
+    public void LogWarning(string message, params object[] args)
+    {
+        var formattedMessage = string.Format(CultureInfo.InvariantCulture, message, args);
+        LogMessages.LogWarningMessage(_logger, formattedMessage);
     }
 }
