6c0d5ca7f5a51f3e46ef7c02c800aa1bfb90d72e
martaf <marta.fullen@iosb-ina.fraunhofer.de>
2024-08-26 09:59:15 +0200

    added copyright headers to .cs files)

--- src/es6numberserializer/NumberFastDToABuilder.cs
+++ src/es6numberserializer/NumberFastDToABuilder.cs
@@ -1,3 +1,16 @@
+/********************************************************************************
+* Copyright (c) {2019 - 2024} Contributors to the Eclipse Foundation
+*
+* See the NOTICE file(s) distributed with this work for additional
+* information regarding copyright ownership.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Apache License Version 2.0 which is available at
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* SPDX-License-Identifier: Apache-2.0
+********************************************************************************/
+
 /* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
