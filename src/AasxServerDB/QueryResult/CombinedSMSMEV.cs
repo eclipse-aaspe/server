f47a700684862205be46bf23f84bb32000cedab4
aorzelskiGH <aorzelski@phoenixcontact.com>
2024-11-28 09:29:07 +0100

    Update Query
    Raw queries for SM, SME and count
    SearchSM with sme
    Run combinedCondition in the end

--- src/AasxServerDB/QueryResult/CombinedSMSMEV.cs
+++ src/AasxServerDB/QueryResult/CombinedSMSMEV.cs
@@ -15,12 +15,24 @@ namespace AasxServerDB
 {
     public partial class Query
     {
-        private class CombinedSMEResult
+        private class CombinedSMSMEV
         {
+            public string? SM_SemanticId { get; set; }
+            public string? SM_IdShort { get; set; }
+            public string? SM_DisplayName { get; set; }
+            public string? SM_Description { get; set; }
             public string? SM_Identifier { get; set; }
-            public string? IdShortPath { get; set; }
-            public string? SME_TimeStamp { get; set; }
+            public DateTime SM_TimeStampTree { get; set; }
+
+            public string? SME_SemanticId { get; set; }
+            public string? SME_IdShort { get; set; }
+            public string? SME_DisplayName { get; set; }
+            public string? SME_Description { get; set; }
+            public string? SME_Id { get; set; }
+            public DateTime SME_TimeStamp { get; set; }
+
             public string? V_Value { get; set; }
+            public double? V_D_Value { get; set; } // needed for the WHERE at the ending 
         }
     }
 }
