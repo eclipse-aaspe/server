5d5eb9f4ea1bdfb24877b4c1ca795cdbc3001b2f
aorzelskiGH <aorzelski@phoenixcontact.com>
2025-06-23 06:54:15 +0200

    Fix publish mqtt

--- src/AasxServerDB/MQTT.cs
+++ src/AasxServerDB/MQTT.cs
@@ -120,7 +120,7 @@ public class SubmodelPublisherService : BackgroundService
                         var json = JsonSerializer.Serialize(changed);
                         await _mqttService.PublishAsync("/noauth/submodels", json);
                         await _mqttService.PublishAsync("/fx/all/submodels", json);
-                        await _mqttService.PublishAsync("/fx/domain/phoenxicontact.com/submodels", json);
+                        await _mqttService.PublishAsync("/fx/domain/phoenixcontact.com/submodels", json);
                         _lastPublish = DateTime.UtcNow;
                     }
                 }
