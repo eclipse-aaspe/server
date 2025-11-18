12626a8a39e11be9bb68197fc1dce003a0d4dfbc
aorzelskiGH <aorzelski2@outlook.com>
2025-11-18 15:39:30 +0100

    Change to local tailwind

---
+++ src/AasxServerBlazor/postcss.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.{razor,cshtml,html}",
+    "./Shared/**/*.{razor,cshtml,html}",
+    "./wwwroot/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
