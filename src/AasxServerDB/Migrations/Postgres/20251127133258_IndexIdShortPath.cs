1b928ea6ec14d6db0a0a02778889c03b4691a0c5
aorzelskiGH <aorzelski@phoenixcontact.com>
2025-11-27 15:03:51 +0100

    Save work

---
+++ src/AasxServerDB/Migrations/Postgres/20251127133258_IndexIdShortPath.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AasxServerDB.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class IndexIdShortPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SMESet_IdShortPath",
+                table: "SMESets",
+                column: "IdShortPath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SMESet_IdShortPath",
+                table: "SMESets");
+        }
+    }
+}
